2ac7d1f51b4d20e241fba0a1326b5707ce5233dc
abhi111676 <abhi111676@gmail.com>
2021-07-07 06:53:04 +0530

    Here we are checking that the employee is present or not

---
+++ PresentAbsent.java
@@ -0,0 +1,14 @@
+
+public class PresentAbsent {
+
+		public static void main(String[] args) {
+
+    int empCheck =(int)Math.floor(Math.random() * 2) ;
+	if(empCheck==0) {
+		System.out.println("Employee is Present");
+	}else {
+		System.out.println("Employee is Absent");
+		}
+	}
+}
+
